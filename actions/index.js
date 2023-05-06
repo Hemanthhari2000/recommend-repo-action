a5450056dd719cff3dde15ab13900c40116d0322
Hemanth <hemanthhari2000@gmail.com>
2023-05-06 19:58:56 +0530

    Add. language filter and construct search query

--- actions/index.js
+++ actions/index.js
@@ -9792,18 +9792,30 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(8661);
 const github = __nccwpck_require__(3033);
 
-try {
-	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput("who-to-greet");
-	console.log(`Hello ${nameToGreet}!`);
-	const time = new Date().toTimeString();
-	core.setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-} catch (error) {
-	core.setFailed(error.message);
+async function run() {
+	try {
+		const nameToGreet = core.getInput("who-to-greet");
+		console.log(`Hello ${nameToGreet}!`);
+		const time = new Date().toTimeString();
+		core.setOutput("time", time);
+		const payload = JSON.stringify(github.context.payload, undefined, 2);
+		console.log(`The event payload: ${payload}`);
+
+		const baseURL =
+			"https://api.github.com/search/issues?q=is:open+issue+label:good-first-issue+bug";
+		const is = "open issue".replace(" ", "+");
+		const label = "good-first-issues bug".replace(" ", "+");
+		const languages = github.context.payload.pull_request.base.repo.language;
+		const searchQuery = `is:${is}+label:${label}+language:${languages}`;
+
+		console.log(`language: ${languages} and type is ${typeof languages}`);
+		console.log(`searchQuery ${searchQuery}`);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
+
+run();
 
 })();
 
