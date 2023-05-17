3243831df2d1711d416d66b10cd22978ee8c3a97
Hemanth <hemanthhari2000@gmail.com>
2023-05-17 12:54:43 +0530

    Refactor. recommended issue list to return strings of links to display in a preview format

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { default: axios } = require("axios");
 
+function getUserAndRepoForTheURL(url) {
+	const urlParts = url.split("/");
+	const user = urlParts[3];
+	const repo = urlParts[4];
+	return { user, repo };
+}
+
 async function run() {
 	try {
 		const baseURL = "https://api.github.com/search/issues?q=";
@@ -12,18 +19,16 @@ async function run() {
 		const searchQuery = `is:${is}+label:${label}+language:${language}`;
 
 		const issueResponse = await axios.get(baseURL + searchQuery);
-
+		
 		const recommendedIssues = [];
-
 		if (issueResponse.status == 200) {
 			issueResponse.data.items.slice(0, 5).forEach((element) => {
-				recommendedIssues.push({
-					url: element.html_url,
-					title: element.title,
-					labels: element.labels.map((element) => element.name),
-					state: element.state,
-					description: element.body.substring(0, 99) + "...",
-				});
+				const { githubUser, githubRepo } = getUserAndRepoForTheURL(
+					element.html_url
+				);
+				recommendedIssues.push(
+					`[![${element.title}](https://github-readme-stats.vercel.app/api/pin/?username=${githubUser}&repo=${githubRepo})](${element.html_url})`
+				);
 			});
 		}
 		console.log(`Recommendations:  ${recommendedIssues}`);
